dfce32be73e9b3e264ef1655fbc4e62c8bcb6db0
Abdirahim Abdi <55918487+Spider13-ui@users.noreply.github.com>
2020-04-14 20:22:27 +0300

    Create scan.py
    Python

---
+++ scan.py
@@ -0,0 +1,6 @@
+number = 0
+for number in range(10):
+    number = number + 1 
+    if number == 5: 
+        pass # pass here print('Number is ' + str(number))
+    print('FB_Found! : 1002578482934561')

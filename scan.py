8ae8dad02c6bbc2fffeeea6f240135d18d40cc30
Abdirahim Abdi <55918487+Spider13-ui@users.noreply.github.com>
2023-01-30 08:50:11 +0300

    Delete scan.py

--- scan.py
+++
@@ -1,6 +0,0 @@
-number = 0
-for number in range(10):
-    number = number + 1 
-    if number == 5: 
-        pass # pass here print('Number is ' + str(number))
-    print('FB_Found! : 1002578482934561')
